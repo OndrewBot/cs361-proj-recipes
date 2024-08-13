cd8cfbb9578ecf432c9cf51d8679aeadd28e26b6
Andrew Bottom <bottoma@oregonstate.edu>
2024-08-13 06:58:44 -0400

    fix for hashmodel error w/ lists

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+import json
 from redis_om import get_redis_connection, HashModel
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -9,58 +9,71 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=['*'],
+    allow_methods=["*"],
     allow_credentials=True,
-    allow_headers=['*']
+    allow_headers=["*"]
 )
 
 redis = get_redis_connection(
     host="red-cqra9k56l47c73ebbuv0",
     port=6379
-)
+    )
 
 class Recipe(HashModel):
     name: str
     description: str
-    ingredients: list[str]  # This will store ingredient names or keys
-    amounts: list[str]  # Amounts corresponding to ingredients
-    units: list[str]  # Units corresponding to ingredients
+    ingredients: str  # Store serialized list
+    amounts: str  # Store serialized list
+    units: str  # Store serialized list
 
     class Meta:
         database = redis
 
-# FastAPI app for managing recipes
 @app.post("/newrecipe")
 def create_recipe(name: str, description: str, ingredients: list[str], amounts: list[str], units: list[str]):
-    recipe = Recipe(name=name, ingredients=ingredients, amounts=amounts, units=units)
+    recipe = Recipe(
+        name=name, 
+        description=description, 
+        ingredients=json.dumps(ingredients),
+        amounts=json.dumps(amounts), 
+        units=json.dumps(units)
+    )
     recipe.save()
     return recipe
 
 @app.get("/recipes/{pk}")
 def get_recipe(pk: str):
-    return format(pk)
+    return format_recipe(pk)
 
 @app.get("/recipes")
-def get_recipe_names(pk: str):
-    return [format_names(pk) for pk in Recipe.all_pks()]
+def get_recipe_names():
+    return [format_recipe_name(pk) for pk in Recipe.all_pks()]
 
-def format(pk: str):
+def format_recipe(pk: str):
     a_recipe = Recipe.get(pk)
-    ingredient_list = []
-    for i in range(len(a_recipe.ingredients)):
-        ingredient_list.append({a_recipe.ingredients[i], a_recipe.amounts[i], a_recipe.units[i]})
+    ingredient_list = json.loads(a_recipe.ingredients)
+    amount_list = json.loads(a_recipe.amounts)
+    unit_list = json.loads(a_recipe.units)
+
+    ingredients = []
+    for i in range(len(ingredient_list)):
+        ingredients.append({
+            "ingredient": ingredient_list[i],
+            "amount": amount_list[i],
+            "unit": unit_list[i]
+        })
 
     return {
-        'id': a_recipe.pk,
-        'name': a_recipe.name,
-        'description': a_recipe.description,
-        'ingredients': ingredient_list
+        "id": a_recipe.pk,
+        "name": a_recipe.name,
+        "description": a_recipe.description,
+        "ingredients": ingredients
     }
 
-def format_names(pk: str):
+def format_recipe_name(pk: str):
     a_recipe = Recipe.get(pk)
     return {
-        'id': a_recipe.pk,
-        'name': a_recipe.name,
-        'description': a_recipe.description
+        "id": a_recipe.pk,
+        "name": a_recipe.name,
+        "description": a_recipe.description
     }
